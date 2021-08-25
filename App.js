8aa5f378b17ce2941cfa89f38c8be413ac1ff24b
Shane Sutro <shanesutro@gmail.com>
2021-08-24 20:56:19 -0600

    Add maps module

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
+  const [ count, setCount ] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>You've clicked { count } times.</Text>
+      <Button
+        onPress={() => setCount(count + 1)}
+        title="Increment"
+      > {count} </Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#fff',
     color: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
